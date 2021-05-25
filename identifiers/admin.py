99cad0c1d217122c8511750574ade7af873549dc
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-05-25 22:06:46 +0100

    update: identifiers app, Identifier model

---
+++ identifiers/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from identifiers.models import Identifier
+
+# Register your models here.
+admin.site.register(Identifier)
---
+++ identifiers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import uuid
+
+
+# Create your models here.
+class Identifier(models.Model):
+    id = models.UUIDField(_('ID'), default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateTimeField(_('Date Created'), auto_now_add=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.created_at)
